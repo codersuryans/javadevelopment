f700a28c24f8908f5af1a2e00dcae14fcad2d89e
codersuryans <mohantysuryans2007@gmail.com>
2025-08-04 20:32:00 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,20 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS
+ */
 import java.util.Scanner;
+
 public class calculator {
+
     public static double add(double a, double b) {
         return a + b;
     }
-
 
     public static double subtract(double a, double b) {
         return a - b;
     }
 
-  
     public static double multiply(double a, double b) {
         return a * b;
     }
 
-    
     public static double divide(double a, double b) {
         if (b == 0) {
             System.out.println("Cannot divide by zero.");
@@ -22,9 +31,9 @@ public class calculator {
         }
         return a / b;
     }
-    
+
     public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         boolean running = true;
 
         while (running) {
@@ -38,32 +47,38 @@ public class calculator {
             System.out.print("Enter choice: ");
             int choice = scanner.nextInt();
 
-            double num2 = scanner.nextDouble();
             if (choice == 5) {
                 running = false;
                 System.out.println("Exiting Calculator. Goodbye!");
                 break;
             }
+
             System.out.print("Enter first number: ");
             double num1 = scanner.nextDouble();
 
             System.out.print("Enter second number: ");
-            switch(choice){
+            double num2 = scanner.nextDouble();
+
+            switch (choice) {
                 case 1:
-                System.out.println("addition is"+add(num1, num2));
+                    System.out.println("Addition is " + add(num1, num2));
+                    break;
                 case 2:
-                System.out.println("substraction is "+subtract(num1, num1));
+                    System.out.println("Subtraction is " + subtract(num1, num2));
+                    break;
                 case 3:
-                                System.out.println("multiplication"+multiply(num1, num1));
+                    System.out.println("Multiplication is " + multiply(num1, num2));
+                    break;
                 case 4:
-                System.out.println("division"+divide(num1, num1));
+                    System.out.println("Division is " + divide(num1, num2));
+                    break;
                 default:
-                System.out.println("invalid switch ");
+                    System.out.println("Invalid choice. Try again.");
             }
         }
 
-
-
+   
     }
 }
+
 
